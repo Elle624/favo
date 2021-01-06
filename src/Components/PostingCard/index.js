1620317d8517e352805728ef51c2e4b08c2b7c9b
Yiran Li <yiran.li@hotmail.com>
2021-01-06 11:26:47 -0700

    Add Link to the entire section

--- src/Components/PostingCard/index.js
+++ src/Components/PostingCard/index.js
@@ -1,32 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './postingCard.scss';
 
 const PostingCard = ({ posting }) => {
   const { id, date, name, organization, openJobs } = posting; 
 
   return (
-    <section key={id} className="posting-wrapper">
-      <div className="vertical">
-        <div className="posting-data">
-          <p className="posting-title left">Organization</p>
-          <p className="posting-detail left">{organization}</p>
+    <Link to={`/postings/:${id}`}>
+      <section key={id} className="posting-wrapper">
+        <div className="vertical">
+          <div className="posting-data">
+            <p className="posting-title left">Organization</p>
+            <p className="posting-detail left">{organization}</p>
+          </div>
+          <div className="posting-data bottom">
+            <p className="posting-title left">Event</p>
+            <p className="posting-detail left">{name}</p>
+          </div>
         </div>
-        <div className="posting-data bottom">
-          <p className="posting-title left">Event</p>
-          <p className="posting-detail left">{name}</p>
+        <div className="vertical">
+          <div className="posting-data">
+            <p className="posting-title right">Date</p>
+            <p className="posting-detail right">{date}</p>
+          </div>
+          <div className="posting-data bottom">
+            <p className="posting-title right">Open Position</p>
+            <p className="posting-detail right">{openJobs.length}</p>
+          </div>
         </div>
-      </div>
-      <div className="vertical">
-        <div className="posting-data">
-          <p className="posting-title right">Date</p>
-          <p className="posting-detail right">{date}</p>
-        </div>
-        <div className="posting-data bottom">
-          <p className="posting-title right">Open Position</p>
-          <p className="posting-detail right">{openJobs.length}</p>
-        </div>
-      </div>
-    </section>
+      </section>
+    </Link>
   )
 
 }
