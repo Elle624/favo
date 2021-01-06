657966ca31b0d007db772d54aed9a1f83766073f
Yiran Li <yiran.li@hotmail.com>
2021-01-06 11:39:59 -0700

    Finish structure html for the section

--- src/Components/PostingView/index.js
+++ src/Components/PostingView/index.js
@@ -4,21 +4,44 @@ import { apiCalls } from "../../apiCalls";
 const PostingView = ({ match }) => {
   const [chosenPosting, setChosenPosting] = useState(null);
   const { id } = match.params;
-
+  
   const getSinglePostingInfo = (id) => {
     apiCalls.getSinglePosting(id).then((data) => setChosenPosting(data))
   }
 
-  useEffect(() => getSinglePostingInfo(id), [])
+  useEffect(() => getSinglePostingInfo(id), []);
 
-  return (
-    <section className="postings-container">
-      <div className="postings-title-wrapper">
-        <h1 className="postings-title">Event Details</h1>
-      </div>
+  if( chosenPosting ) {
+    const { date, name, organization, location, description, duration, openJobs } = chosenPosting;
 
-    </section>
-  )
+    return (
+      <section className="postings-container">
+        <div className="postings-title-wrapper">
+          <h1 className="postings-title">Event Details</h1>
+        </div>
+        <div> 
+          <h3>{name}</h3> 
+          <p>{description}</p>
+          <div>
+            {openJobs.map(job => (
+              <section key={job.id}>
+                <h3>{job.name}</h3>
+                <p>Open Spots: {job.numberOfSpots}</p>
+              </section>
+            ))}
+          </div>
+        </div>
+        <div> 
+          <h3>{date}</h3>
+          <p>Organization: {organization}</p>
+          <p>Location:</p>
+          <p>{location}</p>
+          <p>{duration}</p>
+        </div>
+      </section>
+    )
+  } 
+  return null
   
 }
 
