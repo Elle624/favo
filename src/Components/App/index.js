7e249183a319e9faeb95a215a913fc0595c8ac51
Cooper Terrones <cooper.terrones@outlook.com>
2021-01-05 17:48:49 -0700

    request user info from api

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,7 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
 import "./App.scss";
+import { apiCalls } from "../../apiCalls";
 
 function App() {
-  return <section></section>;
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+  const getUserInfo = () => {
+    apiCalls
+      .getUser()
+      .then((data) => setUser(data))
+      .catch((err) => setError(err.message));
+  };
+
+  useEffect(() => getUserInfo(), []);
+
+  return (
+    <main>
+      <p>{user.name}</p>
+    </main>
+  );
 }
 
 export default App;
