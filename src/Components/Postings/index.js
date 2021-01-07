d89293e1728c94e6ade68232e2bf56c0a7105907
Yiran Li <yiran.li@hotmail.com>
2021-01-07 15:01:28 -0700

    Get categories and pass to Navigation component

--- src/Components/Postings/index.js
+++ src/Components/Postings/index.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Postings.scss';
 import PostingCard from '../PostingCard';
 import Navigation from "../Navigation";
 
 const Postings = ({ postings, searchByKeyWord }) => {
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = () => {
+    const categories = postings.reduce((totalCategories, posting) => {
+      if (!totalCategories.includes(posting.category)) {
+        totalCategories.push(posting.category)
+      }
+      return totalCategories;
+    }, []);
+    setCategories(categories);
+  } 
+
   const postingCards = postings.map(posting => (
-    <PostingCard posting={posting}/>
+    <PostingCard posting={posting} />
   ))
-return (
-  <section className="postings-container">
-    <div className="postings-title-wrapper">
-      <h1 className="postings-title">Open Volunteer Positions</h1>
-    </div>
-    <div>
-      <Navigation searchByKeyWord={searchByKeyWord} />
-    </div>
-    <section className="postings-wrapper">
-      {postingCards}
+
+  useEffect(() => getCategories(), [])
+  
+  return (
+    <section className="postings-container">
+      <div className="postings-title-wrapper">
+        <h1 className="postings-title">Open Volunteer Positions</h1>
+      </div>
+      <div>
+        <Navigation searchByKeyWord={searchByKeyWord} categories={categories}/>
+      </div>
+      <section className="postings-wrapper">
+        {postingCards}
+      </section>
     </section>
-  </section>
-)
+  )
 }
 
 export default Postings
