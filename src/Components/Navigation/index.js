f4711b1b7b38ab41edd8d8433fdc7c57897d35a0
Yiran Li <yiran.li@hotmail.com>
2021-01-06 20:39:56 -0700

    Create searchPostings function for search feature

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 
 const Navigation = ({ postings }) => {
   const [keyWord, setKeyWord] = useState('');
-  const [searchedPostings, setSearchedPostings] = useState([])
+  const [searchedPostings, setSearchedPostings] = useState([]);
 
+  const searchPostings = (e) => {
+    e.preventDefault();
+    const lowerCaseKeyword = keyWord.toLowerCase();
+    const filteredPostings = postings.filter(posting => posting.name.toLowerCase().includes(keyWord) || posting.organization.toLowerCase().includes(keyWord));
+    setSearchedPostings(filteredPostings);
+    setKeyWord('');
+  }
+  
   return(
     <section>
-      <input type="text" value={keyWord} placeholder="keyword"></input>
-      <input type="submit" value="search"></input>
+      <input type="text" value={keyWord} placeholder="keyword" onChange={(e) => setKeyWord(e.target.value)}></input>
+      <input type="submit" value="search" onClick={searchPostings}></input>
     </section>
   )
 
