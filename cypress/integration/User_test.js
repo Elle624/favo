2f88754304a89b20705ac19abf000354d83f01a5
scripka <olga.morgan17@gmail.com>
2021-01-07 20:45:58 -0700

    Add test for a user sidebar: check basic info

--- cypress/integration/User_test.js
+++ cypress/integration/User_test.js
@@ -1,11 +1,36 @@
 
 describe('Testing homepage', () => {
-  
-  it("Landing on the homepage with on load", () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/')
+  })
+
+  it("Landing on the homepage you can see user sidebar with all user basic information", () => {
+
+    cy.intercept({
+      method: 'GET',
+      url: 'http://localhost:3001/users/1'
+    }, {
+      statusCode: 201,
+      body: {
+        
+              id: 1,
+              name: "Peach Perfect",
+              rating: 1.4,
+              profilePicture: 'https://randomuser.me/api/portraits/men/52.jpg',
+              volunteeredHours: 8.2,
+              upcomingJobs: [
+                {
+                  id: '1-posting-23',
+                  eventName: 'Help students to have a dinner',
+                  positionName: 'cook',
+                  date: '2021/03/01'
+                }
+              ]  
+      }
+    })
 
     cy.get('.username')
-      .contains('Wheezy Joe')
+      .contains('Peach Perfect')
 
     cy.get('.profile-picture')
       .should('have.css', 'background-image', 'url("https://randomuser.me/api/portraits/men/52.jpg")')
@@ -16,7 +41,7 @@ describe('Testing homepage', () => {
       .get('.sidebar-titles').contains('My Upcoming Jobs')
       .get('.section-line').should('have.length', 2)
 
-    cy.get('.hours-bar').contains('10.2 Hours')
+    cy.get('.hours-bar').contains('8.2 Hours')
   })
 })
 
