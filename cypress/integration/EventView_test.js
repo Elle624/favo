5ec0c0f8a6a80fc809f907ea9798e00427930240
Yiran Li <yiran.li@hotmail.com>
2021-01-09 15:28:52 -0700

    Add test for testing rendering details

--- cypress/integration/EventView_test.js
+++ cypress/integration/EventView_test.js
@@ -2,52 +2,35 @@ import _mockData from "./_ mockData";
 
 describe("Testing the single event details page", () => {
   beforeEach(() => {
-    // cy.visit('http://localhost:3000/postings/event-1');
-
-    // cy.intercept(
-    //   {
-    //     method: "GET",
-    //     url: "http://localhost:3001/events/event-1",
-    //   },
-    //   {
-    //     statusCode: 201,
-    //     body: _mockData.events[0],
-    //   }
-    // );
-  })
-
-  it('should render correctly', () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://localhost:3000/postings/event-1');
+    
     cy.intercept(
       {
         method: "GET",
-        url: "http://localhost:3001/events",
+        url: "http://localhost:3001/events/event-1",
       },
       {
         statusCode: 201,
-        body: _mockData.events,
+        body: _mockData.events[0],
       }
     );
-    cy.get(".posting-wrapper")
-    // .contains("Something Crazy")
-    // .click()
+  })
 
-    //cy.visit('http://localhost:3000/postings/event-1');
-    // cy.intercept(
-    //   {
-    //     method: "GET",
-    //     url: "http://localhost:3001/events/event-1",
-    //   },
-    //   {
-    //     statusCode: 201,
-    //     body: _mockData.events[0],
-    //   }
-    // );
-    // cy.get(".posting-detail")
-      // .should("contain", "Something Crazy")
-      // .and("contain", "Something Else, LLC")
-      // .and("contain", 3)
-      // .and("contain", "2021/02/01");
+  it('should render event details correctly', () => {
+    cy.get(".postings-container")
+      .should("contain", "Something Crazy")
+      .and("contain", "Description")
+      .and("contain", "Something crazy happening somewhere in the somewhere of somewhere CO. Come do something with someone.")
+      .and("contain", "Open Positions")
+      .and("contain", "Feb 01 2021")
+      .and("contain", "Organization")
+      .and("contain", "Something Else, LLC")
+      .and("contain", "Category")
+      .and("contain", "Something")
+      .and("contain", "Location")
+      .and("contain", "123 Something Dr., Somewhere, CO, 00000")
+      .and("contain", "Duration")
+      .and("contain", "10")
   })
 
 })
