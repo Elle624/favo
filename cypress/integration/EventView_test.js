96d0a14d9f23f6d3c5966360bb8e885904f36acc
Yiran Li <yiran.li@hotmail.com>
2021-01-09 16:07:49 -0700

    Create test for signing up job function

--- cypress/integration/EventView_test.js
+++ cypress/integration/EventView_test.js
@@ -64,5 +64,50 @@ describe("Testing the single event details page", () => {
     cy.url().should("include", "/")
   })
 
+  it.only("Should display upcoming jobs and able to sign up", () => {
+    cy.get(".posting-position-cards-wrapper")
+      .should("contain", "assisting with check-in")
+      .and("contain", "Open Spots:")
+      .and("contain", "3")
+      .and("contain", "tossing color")
+      .and("contain", "5")
+      .and("contain", "handing out water")
+      .and("contain", "6");
+    
+    cy.get(".posting-positions-card:first")
+      .focus()
+      .should("have.css", "box-shadow", "rgb(46, 196, 182) 2px 2px 3px 0px")
+      .get(".submit-button")
+      .should("contain", "Sign me up!")
+      .click();
+
+    cy.intercept(
+      {
+        method: "PATCH",
+        url: "http://localhost:3001/events/event-1",
+      },
+      {
+        statusCode: 201,
+        body: {jobId: "posting-1"},
+      }
+    );
+    cy.intercept(
+      {
+        method: "POST",
+        url: "http://localhost:3001/users/1",
+      },
+      {
+        statusCode: 201,
+        body: {
+          id: "posting-1",
+          eventId: "event-1",
+          eventName: "Something Crazy",
+          positionName: "assisting with check-in",
+          date: "2021/02/01"
+        },
+      }
+    );
+  })
+
 
 })
