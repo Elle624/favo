484f26f088f0e4bae4ee2e6a51b01e01d117b9c0
Yiran Li <yiran.li@hotmail.com>
2021-01-10 15:56:40 -0700

    Update and add more pairs to mock data

--- src/TestData/_mockData.js
+++ src/TestData/_mockData.js
@@ -1,20 +1,22 @@
 const _mockData = {
-  users:[{
-    id: 1,
-    name: "Peach Perfect",
-    rating: 1.4,
-    profilePicture: "https://randomuser.me/api/portraits/men/52.jpg",
-    volunteeredHours: 8.2,
-    upcomingJobs: [
-      {
-        id: "1-posting-30",
-        eventId: "event-20",
-        eventName: "Something Crazy",
-        positionName: "cook",
-        date: "2021/03/01",
-      },
-    ],
-  }],
+  users: [
+    {
+      id: 1,
+      name: "Peach Perfect",
+      rating: 1.4,
+      profilePicture: "https://randomuser.me/api/portraits/men/52.jpg",
+      volunteeredHours: 8.2,
+      upcomingJobs: [
+        {
+          id: "1-posting-30",
+          eventId: "event-20",
+          eventName: "Something Crazy",
+          positionName: "cook",
+          date: "2021/03/01"
+        }
+      ]
+    }
+  ],
   events: [
     {
       id: "event-20",
@@ -67,6 +69,42 @@ const _mockData = {
       ],
     },
   ],
+  eventId: {
+    params: {
+      id: "event-2"
+    }
+  },
+
+  postJobBody: {
+    id: "1-posting-4",
+    eventId: "event-2",
+    eventName: "Food Delivery",
+    positionName: "driver",
+    date: "2021/02/05"
+  },
+
+  updatedEvent: {
+    id: "event-2",
+    date: "2021/02/05",
+    name: "Food Delivery",
+    organization: "Individual",
+    location: "123 Goose Blv., Denver, CO, 80208",
+    description: "Deliver food for a memorial hospital",
+    duration: 3,
+    category: "Healthcare",
+    openJobs: [
+      {
+        id: "posting-4",
+        name: "driver",
+        numberOfSpots: 1,
+      },
+      {
+        id: "posting-5",
+        name: "cook",
+        numberOfSpots: 3,
+      },
+    ]
+  }
 };
 
 export default _mockData;
