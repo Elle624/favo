3ca2192549b870e7df7c6269cfd832d1f2a6d07c
Yiran Li <yiran.li@hotmail.com>
2021-01-10 13:05:34 -0700

    Add render correctly test

--- src/Components/User/User.test.js
+++ src/Components/User/User.test.js
@@ -1,14 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { _mockData } from '../../TestData/_ mockData';
+import _mockData from '../../TestData/_mockData';
 import User from '../User';
+import { MemoryRouter } from 'react-router-dom';
 
 describe("User Comonent", () => {
 
-  it("should render correctly", () => {
-    render(<User />);
+  it("should render correctly", () => {    
+    render(<User info={_mockData.user}/>, { wrapper: MemoryRouter });
 
-    expect(screen.getByText("Peach Perfect")).toBeInTheDocument;
-
+    expect(screen.getByText("Peach Perfect")).toBeInTheDocument();
+    expect(screen.getAllByAltText("star-icon")).toHaveLength(5);
+    expect(screen.getByText("Total Hours Volunteered")).toBeInTheDocument();
+    expect(screen.getByText("8.2 Hours")).toBeInTheDocument();
+    expect(screen.getByText("My Upcoming Jobs")).toBeInTheDocument();
+    expect(screen.getByText("Something Crazy")).toBeInTheDocument();
+    expect(screen.getByText("2021/03/01")).toBeInTheDocument();
+    expect(screen.getByText("cook")).toBeInTheDocument();
+    expect(screen.queryByText("Food Delivery")).not.toBeInTheDocument();
   })
 })
