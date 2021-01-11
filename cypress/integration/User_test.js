e7325df70f52231b3de9978fe6969a530b41368e
Yiran Li <yiran.li@hotmail.com>
2021-01-10 17:45:43 -0700

    Update user tests to test server

--- cypress/integration/User_test.js
+++ cypress/integration/User_test.js
@@ -3,21 +3,11 @@ import _mockData from "./_ mockData";
 describe("Testing homepage", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
-
-    cy.intercept(
-      {
-        method: "GET",
-        url: "http://localhost:3001/users/1",
-      },
-      {
-        statusCode: 201,
-        body: _mockData.user,
-      }
-    );
+    cy.intercept("https://ivolunteer-api-test.herokuapp.com/users/1");
   });
 
   it("Landing on the homepage you can see user sidebar with all user basic information", () => {
-    cy.get(".username").contains("Peach Perfect");
+    cy.get(".username").contains("Wheezy Joe");
 
     cy.get(".profile-picture").should(
       "have.css",
@@ -34,28 +24,28 @@ describe("Testing homepage", () => {
       .get(".section-line")
       .should("have.length", 2);
 
-    cy.get(".hours-bar").contains("8.2 Hours");
+    cy.get(".hours-bar").contains("10.2 Hours");
   });
 
   it("Should display user upcoming jobs on the user profile sidebar", () => {
     cy.get(".job-event-main-detail")
-      .contains("cook")
+      .contains("dog walkers")
       .get(".job-event-main-detail")
       .contains("2021/03/01")
       .get(".job-event-name")
-      .contains("Help students to have a dinner");
+      .contains("Help a local animal shelter");
   });
 
   it("Should display toggle sidebar button that hides it", () => {
     cy.get(".user-toggle-button img").should("be.visible").click();
 
-    cy.get(".username").contains("Peach Perfect").should("not.be.visible");
+    cy.get(".username").contains("Wheezy Joe").should("not.be.visible");
   });
 
   it("By clicking on an upcoming job the websites sends the user to the event page", () => {
     cy.get(".upcoming-job-card")
       .click()
       .url()
-      .should("include", "/postings/event-20");
+      .should("include", "/postings/event-10");
   });
 });
