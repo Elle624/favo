52435ecf3f6ee30d101e59d708c3cd3007d93040
Yiran Li <yiran.li@hotmail.com>
2021-01-10 17:56:42 -0700

    Add test api url to postings tests and refactor

--- cypress/integration/Postings_test.js
+++ cypress/integration/Postings_test.js
@@ -1,19 +1,10 @@
 import _mockData from "./_ mockData";
 
 describe("Testing Postings component", () => {
-  beforeEach(() => {
-    // cy.visit("http://localhost:3000/");
 
-    // cy.intercept(
-    //   {
-    //     method: "GET",
-    //     url: "http://localhost:3001/events",
-    //   },
-    //   {
-    //     statusCode: 200,
-    //     body: _mockData.events,
-    //   }
-    // );
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.intercept("https://ivolunteer-api-test.herokuapp.com/events");
   });
 
   it("Landing on the homepage you can see all event postings", () => {
@@ -24,93 +15,76 @@ describe("Testing Postings component", () => {
       .and("contain", "Open Position");
 
     cy.get(".posting-detail")
-      .should("contain", "Something Crazy")
-      .and("contain", "Something Else, LLC")
+      .should("contain", "The Color Run")
+      .and("contain", "Color Run")
       .and("contain", 3)
       .and("contain", "2021/02/01");
   });
 
   it("Should load title of page on homepage", () => {
-    cy.get(".postings-title").contains("Open Volunteer Positions");
+    cy.get(".postings-title")
+      .contains("Open Volunteer Positions");
   });
 
   it("Posting card CSS", () => {
-    cy.get(".posting-wrapper-cards").should(
-      "have.css",
-      "border",
-      "1px solid rgb(255, 191, 105)"
-    );
+    cy.get(".posting-wrapper-cards")
+      .should(
+        "have.css",
+        "border",
+        "1px solid rgb(255, 191, 105)"
+      );
 
-    cy.get(".posting-wrapper-cards").should("have.css", "display", "flex");
-
-    cy.get(".posting-wrapper-cards").should("have.css", "flex-direction", "row");
+    cy.get(".posting-wrapper-cards")
+      .should("have.css", "display", "flex")
+      .and("have.css", "flex-direction", "row");
   });
 
   it("Search Input should load and function on homepage postings", () => {
-    cy.get(".input-button-sort").should(
-      "have.attr",
-      "placeholder",
-      "i.e Boulder..."
-    );
-
-    cy.get(".button-search").contains("search");
-
-    cy.get(".input-button-sort").type("Something");
-    cy.get(".button-search").click();
-
-    cy.get(".posting-detail").should("contain", "Something Crazy");
-
-    cy.get(".posting-detail").should("not.contain", "Food Devlivery");
+    cy.get(".input-button-sort")
+      .should(
+        "have.attr",
+        "placeholder",
+        "i.e Boulder..."
+      )
+      .type("Color")
+      .get(".button-search")
+      .contains("search")
+      .click();
+      
+    cy.get(".posting-detail")
+      .should("contain", "Color Run")
+      .and("not.contain", "Individual");
   });
 
   it("Sort button should load and function on homepage", () => {
-    cy.get(".button-sort").contains("sort");
+    cy.get(".button-sort")
+      .contains("sort")
+      .click();
 
-    cy.get(".button-sort").click();
-
-    cy.get(".posting-wrapper-cards:first").contains("Food Delivery");
-    cy.get(".posting-wrapper-cards:last").contains("Something Crazy");
+    cy.get(".posting-wrapper-cards:first")
+      .contains("The Women of Valor Program")
+      .get(".posting-wrapper-cards:last")
+      .contains("The Color Run");
   });
 
   it("Filter postings by categories on should load and function on", () => {
-    cy.get(".filter-item").contains("-- select category --");
+    cy.get(".filter-item")
+      .contains("-- select category --");
 
-    cy.get(".filter-box").select("Something");
+    cy.get(".filter-box")
+      .select("Animal");
 
-    cy.get(".posting-wrapper-cards").should("contain", "Something Crazy");
-    cy.get(".posting-detail").should("not.contain", "Food Devlivery");
+    cy.get(".posting-wrapper-cards")
+      .should("contain", "Dumb friends")
+      .get(".posting-detail")
+      .should("not.contain", "Food Devlivery");
   });
 
-  it.only("should direct to a new URL when a posting card is clicked", () => {
-
-    cy.visit("http://localhost:3000/");
-
-    // cy.intercept(
-    //   {
-    //     method: "GET",
-    //     url: "http://localhost:3001/events/event-1",
-    //   },
-    //   {
-    //     statusCode: 200,
-    //     body: _mockData.events[0],
-    //   }
-    // );
-      
-
-    cy.intercept(
-      {
-        method: "GET",
-        url: "http://localhost:3001/events",
-      },
-      {
-        statusCode: 200,
-        body: _mockData.events,
-      }
-    );
-
+  it("should direct to a new URL when a posting card is clicked", () => {
     cy.get(".posting-wrapper-cards:first")
       .click()
       .url()
       .should("include", "/postings/event-1");
   });
+  
 });
