dd22038383ef5f9e78303319186fa4d9b475485f
scripka <olga.morgan17@gmail.com>
2021-01-11 20:42:58 -0700

    Add homepage btn to assets and create basic sctructure for the homepage

--- src/Components/WelcomePage/index.js
+++ src/Components/WelcomePage/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./WelcomePage.scss";
-import backButton from "../../Assets/back-button.png";
+import homepageButton from "../../Assets/homepage-button.png";
 
 const WelcomePage = () => {
   return(
-    <p>Hello</p>
+    <section className="welcome-page-container">
+      <div className="welcome-page-titles-wrapper">
+        <h1 className="welcome-page-title">Welcome to iVolunteer!</h1>
+        <h2 className="welcome-page-tagline">JUST DO IT!</h2>
+      </div>
+      <div>
+        <p>go to your profile</p>
+        <Link className="welcome-page-link">
+          <img className="welcome-page-button" src={homepageButton} alt="homepage-button"/>
+        </Link>
+      </div>
+
+    </section>
   )
 
 }
