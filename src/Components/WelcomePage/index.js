286e8c48f5c2f844f5b036cdba08c18e26771f20
scripka <olga.morgan17@gmail.com>
2021-01-11 21:18:17 -0700

    Add basic style and animation, change button and tagline

--- src/Components/WelcomePage/index.js
+++ src/Components/WelcomePage/index.js
@@ -7,19 +7,17 @@ const WelcomePage = () => {
   return(
     <section className="welcome-page-container">
       <div className="welcome-page-titles-wrapper">
-        <h1 className="welcome-page-title">Welcome to iVolunteer!</h1>
-        <h2 className="welcome-page-tagline">JUST DO IT!</h2>
+        <h1 className="welcome-page-title">Welcome to FaVo!</h1>
+        <h2 className="welcome-page-tagline">toBee or not toBee, it’s your choice…</h2>
       </div>
-      <div>
-        <p>go to your profile</p>
-        <Link className="welcome-page-link">
+      <div className="welcome-page-homepage-wrapper">
+        <p className="welcome-page-button-text">go to your profile</p>
+        <Link to="/postings" className="welcome-page-link">
           <img className="welcome-page-button" src={homepageButton} alt="homepage-button"/>
         </Link>
       </div>
-
     </section>
   )
-
 }
 
 export default WelcomePage;
