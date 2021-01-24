a8846d2feae87b59b57d3b7010c986203c5d488e
Yiran Li <yiran.li@hotmail.com>
2021-01-23 17:04:05 -0700

    Fix upcoming job now can link to event details page

--- src/Components/PostingView/index.js
+++ src/Components/PostingView/index.js
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { apiCalls } from "../../apiCalls";
-import "./PostingView.scss";
-import backButton from "../../Assets/back-button.png";
-import { Link } from "react-router-dom";
-import Loading from "../Loading";
+import React, { useState, useEffect } from 'react';
+import { apiCalls } from '../../apiCalls';
+import './PostingView.scss';
+import backButton from '../../Assets/back-button.png';
+import { Link } from 'react-router-dom';
+import Loading from '../Loading';
 
 const PostingView = ({ match, getUserInfo }) => {
   const eventId = match.params.id;
   const [chosenPosting, setChosenPosting] = useState(null);
   const [chosenJob, setChosenJob] = useState(null);
-  const [signedUpJobName, setSignedUpJobName] = useState("");
+  const [signedUpJobName, setSignedUpJobName] = useState('');
   const [userInfo, setUserInfo] = useState(null);
+  let isMounted = false;
 
   const getDetails = () => {
+    isMounted = true;
     Promise.all([apiCalls.getUser(), apiCalls.getSinglePosting(eventId)]).then(
       (data) => {
-        if (data[0]) {
+        if (isMounted && data[0]) {
           setUserInfo(data[0]);
           setChosenPosting(data[1]);
           const signedUpEvent = data[0].upcomingJobs.find(
@@ -43,14 +45,17 @@ const PostingView = ({ match, getUserInfo }) => {
       eventId,
       eventName: chosenPosting.name,
       positionName: chosenJob.name,
-      date: chosenPosting.date,
+      date: chosenPosting.date
     };
     apiCalls.postJobPosting(newUpcomingJob).then(() => {
       getUserInfo();
     });
   };
 
-  useEffect(() => getDetails(), []);
+  useEffect(() => {
+    getDetails();
+    return () => (isMounted = false);
+  }, [userInfo]);
 
   if (chosenPosting) {
     const {
@@ -61,111 +66,111 @@ const PostingView = ({ match, getUserInfo }) => {
       description,
       duration,
       category,
-      openJobs,
+      openJobs
     } = chosenPosting;
 
     const reformedDate = new Date(date)
       .toDateString()
-      .split(" ")
+      .split(' ')
       .slice(1)
-      .join(" ");
+      .join(' ');
 
     return (
       <section
-        data-testid="posting-view-element"
-        className="postings-container-single"
+        data-testid='posting-view-element'
+        className='postings-container-single'
       >
-        <div className="postings-title-wrapper">
-          <h1 className="postings-title">Event Details</h1>
+        <div className='postings-title-wrapper'>
+          <h1 className='postings-title'>Event Details</h1>
         </div>
-        <div className="posting-info-wrapper">
-          <div className="posting-left-info-wrapper">
-            <div className="back-button-wrap">
-              <Link to="/postings">
+        <div className='posting-info-wrapper'>
+          <div className='posting-left-info-wrapper'>
+            <div className='back-button-wrap'>
+              <Link to='/postings'>
                 <img
                   src={backButton}
-                  className="back-button-img"
-                  alt="return-home-button"
+                  className='back-button-img'
+                  alt='return-home-button'
                 />
               </Link>
             </div>
-            <h3 className="event-title">{name}</h3>
-            <div className="section-titles">
+            <h3 className='event-title'>{name}</h3>
+            <div className='section-titles'>
               <strong>
-                <p className="posting-info-title">Description</p>
+                <p className='posting-info-title'>Description</p>
               </strong>
-              <hr className="section-line" />
+              <hr className='section-line' />
             </div>
-            <p className="event-description">{description}</p>
-            <div className="section-titles">
+            <p className='event-description'>{description}</p>
+            <div className='section-titles'>
               <strong>
-                <p className="posting-info-title">Open Positions</p>
+                <p className='posting-info-title'>Open Positions</p>
               </strong>
-              <hr className="section-line" />
+              <hr className='section-line' />
             </div>
-            <div className="posting-position-cards-wrapper">
+            <div className='posting-position-cards-wrapper'>
               {openJobs.map((job) => (
                 <button
                   onClick={() => setChosenJob(job)}
                   key={job.id}
-                  className="posting-positions-card"
+                  className='posting-positions-card'
                   style={{
                     backgroundColor:
-                      signedUpJobName === job.name ? "#2ec4b6" : "#initial",
-                    color: signedUpJobName === job.name ? "white" : "#initial",
+                      signedUpJobName === job.name ? '#2ec4b6' : '#initial',
+                    color: signedUpJobName === job.name ? 'white' : '#initial',
                     borderColor:
-                      signedUpJobName === job.name ? "#2ec4b6" : "#initial",
+                      signedUpJobName === job.name ? '#2ec4b6' : '#initial'
                   }}
                 >
-                  <h3 className="event-job-name">{job.name}</h3>
-                  <p className="event-job-title">
+                  <h3 className='event-job-name'>{job.name}</h3>
+                  <p className='event-job-title'>
                     Open Spots: {job.numberOfSpots}
                   </p>
                 </button>
               ))}
             </div>
-            <div className="submit-button-wrapper">
+            <div className='submit-button-wrapper'>
               <button
                 onClick={substractOpenPosition}
                 disabled={signedUpJobName ? true : false}
-                style={{ display: signedUpJobName ? "none" : "block" }}
-                className="submit-button"
+                style={{ display: signedUpJobName ? 'none' : 'block' }}
+                className='submit-button'
               >
                 Sign me up!
               </button>
             </div>
-            <div className="sign-up-event-message-wrapper">
+            <div className='sign-up-event-message-wrapper'>
               <p
-                style={{ display: signedUpJobName ? "inline-block" : "none" }}
-                className="sign-up-event-message"
+                style={{ display: signedUpJobName ? 'inline-block' : 'none' }}
+                className='sign-up-event-message'
               >
                 You have already signed up for this event!
               </p>
             </div>
           </div>
-          <div className="posting-right-info-wrapper">
-            <h3 className="event-title">{reformedDate}</h3>
-            <div className="posting-organization-wrapper">
+          <div className='posting-right-info-wrapper'>
+            <h3 className='event-title'>{reformedDate}</h3>
+            <div className='posting-organization-wrapper'>
               <strong>
-                <p className="posting-info-title">Organization</p>
+                <p className='posting-info-title'>Organization</p>
               </strong>
               <p>{organization}</p>
             </div>
-            <div className="posting-category-wrapper">
+            <div className='posting-category-wrapper'>
               <strong>
-                <p className="posting-info-title">Category</p>
+                <p className='posting-info-title'>Category</p>
               </strong>
               <p>{category}</p>
             </div>
-            <div className="posting-location-wrapper">
+            <div className='posting-location-wrapper'>
               <strong>
-                <p className="posting-info-title">Location</p>
+                <p className='posting-info-title'>Location</p>
               </strong>
               <p>{location}</p>
             </div>
-            <div className="posting-duration-wrapper">
+            <div className='posting-duration-wrapper'>
               <strong>
-                <p className="posting-info-title">Duration</p>
+                <p className='posting-info-title'>Duration</p>
               </strong>
               <p>{duration}</p>
             </div>
