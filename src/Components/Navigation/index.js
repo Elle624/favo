9cc64473b86d244fa13fd675979003212f7a8329
Yiran Li <yiran.li@hotmail.com>
2021-01-26 19:57:12 -0700

    Toggle reset button in nav section

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
-import "./Navigation.scss";
-import descSortButton from "../../Assets/desc-sort-button.png";
-import asceSortButton from "../../Assets/asce-sort-button.png";
+import React, { useState } from 'react';
+import './Navigation.scss';
+import descSortButton from '../../Assets/desc-sort-button.png';
+import asceSortButton from '../../Assets/asce-sort-button.png';
 
 const Navigation = ({
   isSorted,
   searchByKeyWord,
   categories,
   filterByCategory,
-  sortByDate,
+  sortByDate
 }) => {
-  const [keyWord, setKeyWord] = useState("");
+  const [keyWord, setKeyWord] = useState('');
+  const [isReset, setReset] = useState(false);
 
   const searchPostings = (e) => {
     e.preventDefault();
     searchByKeyWord(keyWord);
-    setKeyWord("");
+    setKeyWord('');
+    setReset(true);
   };
 
   const sortPostings = (e) => {
@@ -23,51 +25,56 @@ const Navigation = ({
     sortByDate();
   };
 
+  const filterPostings = (e) => {
+    filterByCategory(e.target.value);
+    setReset(true);
+  };
+
   return (
-    <section data-testid="navigation-element" className="navigation-bar">
-      <label htmlFor="search-input" className="label-input-search"></label>
+    <section data-testid='navigation-element' className='navigation-bar'>
+      <label htmlFor='search-input' className='label-input-search'></label>
       <input
-        id="search-input"
-        name="search-input"
-        className="input-button-sort"
-        type="text"
+        id='search-input'
+        name='search-input'
+        className='input-button-sort'
+        type='text'
         value={keyWord}
-        placeholder="i.e Boulder..."
+        placeholder='i.e Boulder...'
         onChange={(e) => setKeyWord(e.target.value)}
       ></input>
-      <button className="button-search" type="submit" onClick={searchPostings}>
+      <button className='button-search' type='submit' onClick={searchPostings}>
         search
       </button>
-      <div className="container-button-sort">
+      <div className='container-button-sort'>
         <button
           onClick={sortPostings}
-          className="button-sort"
-          value="sort"
-          type="submit"
+          className='button-sort'
+          value='sort'
+          type='submit'
         >
           sort
-          <span aria-hidden="true" className="glyphicon">
+          <span aria-hidden='true' className='glyphicon'>
             <img
-              className="sort-icon"
+              className='sort-icon'
               src={isSorted ? asceSortButton : descSortButton}
-              alt="sort-icon"
+              alt='sort-icon'
             />
           </span>
         </button>
       </div>
-      <article className="container-button-filter">
+      <article className='container-button-filter'>
         <select
-          data-testid="select-input"
-          className="filter-box"
-          name="category"
-          onChange={(e) => filterByCategory(e.target.value)}
+          data-testid='select-input'
+          className='filter-box'
+          name='category'
+          onChange={filterPostings}
         >
-          <option className="filter-item" defaultValue>
+          <option className='filter-item' defaultValue>
             -- select category --
           </option>
           {categories.map((category) => (
             <option
-              className="filter-item"
+              className='filter-item'
               value={category}
               key={`1-${category}`}
             >
@@ -76,6 +83,13 @@ const Navigation = ({
           ))}
         </select>
       </article>
+      <button
+        className='button-search'
+        type='submit'
+        style={{ display: isReset ? 'block' : 'none' }}
+      >
+        reset
+      </button>
     </section>
   );
 };
